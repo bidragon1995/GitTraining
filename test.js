160caf0c7ae9f64b7df5c7edec3a98debf884d70
dkbi <bidragon007@gmail.com>
2021-04-11 15:48:13 +0700

    Add content to client

--- test.js
+++ test.js
@@ -3,5 +3,6 @@ var http = require("http");
 
 var server = http.createServer( function (req, res) {
     res.write("Hello !!! I'm  Git");
+    res.write("Edited by Bi Dragon");
     res.end();
 }).listen(3000);
