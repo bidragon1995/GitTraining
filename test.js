c0ab6b081d839aee3f55d7592bded5cf6780f138
dkbi <bidragon007@gmail.com>
2021-04-11 15:47:26 +0700

    Create Http Server

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("Hello Girls");
+var http = require("http");
+
+var server = http.createServer( function (req, res) {
+    res.write("Hello !!! I'm  Git");
+    res.end();
+}).listen(3000);
